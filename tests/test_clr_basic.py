740fb95e58590d5337849b53bf0ab5eaf6da7e64
david <dale.david@mail.ru>
2023-10-31 23:15:02 +0100

    add Github autotests; change the test dataset

--- tests/test_clr_basic.py
+++ tests/test_clr_basic.py
@@ -1,21 +1,24 @@
 import numpy as np
 from constrained_linear_regression import ConstrainedLinearRegression
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_linnerud
 from sklearn.linear_model import LinearRegression
 
 
 def test_unconstrained():
-    X, y = load_boston(return_X_y=True)
+    X, Y = load_linnerud(return_X_y=True)
+    y = Y[:, 0]
     model = ConstrainedLinearRegression(nonnegative=False)
     model.fit(X, y)
     baseline = LinearRegression()
     baseline.fit(X, y)
+    assert model.coef_.min() < 0
     assert np.allclose(baseline.coef_, model.coef_)
     assert np.isclose(baseline.intercept_, model.intercept_)
 
 
 def test_positive():
-    X, y = load_boston(return_X_y=True)
+    X, Y = load_linnerud(return_X_y=True)
+    y = Y[:, 0]
     model = ConstrainedLinearRegression(nonnegative=True)
     model.fit(X, y)
     assert np.all(model.coef_ >= 0)
